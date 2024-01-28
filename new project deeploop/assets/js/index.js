12d4aa05d195be159977241973a5ad7280f1fcc0
Abhishek Tyagi <abhishektyagi2912@gmail.com>
2024-01-28 17:33:02 +0530

    error:fix

--- new project deeploop/assets/js/index.js
+++ new project deeploop/assets/js/index.js
@@ -6,168 +6,185 @@ import { preloadImages } from './utils.js';
 
 let lenis;
 
+let lastScroll = 0;
+const navbar = document.querySelector('.home-container');
+
+window.addEventListener('scroll', () => {
+    const currentScroll = window.scrollY;
+
+    if (currentScroll > lastScroll) {
+        // Scroll down
+        navbar.classList.add('hidden');
+    } else {
+        // Scroll up
+        navbar.classList.remove('hidden');
+    }
+
+    lastScroll = currentScroll;
+});
+
 // Function to initialize Lenis for smooth scrolling
 const initSmoothScrolling = () => {
-	// Instantiate the Lenis object with specified properties
-	lenis = new Lenis({
-		lerp: 0.1, // Lower values create a smoother scroll effect
-		smoothWheel: true // Enables smooth scrolling for mouse wheel events
-	});
+    // Instantiate the Lenis object with specified properties
+    lenis = new Lenis({
+        lerp: 0.1, // Lower values create a smoother scroll effect
+        smoothWheel: true // Enables smooth scrolling for mouse wheel events
+    });
 
-	// Update ScrollTrigger each time the user scrolls
-	lenis.on('scroll', () => ScrollTrigger.update());
+    // Update ScrollTrigger each time the user scrolls
+    lenis.on('scroll', () => ScrollTrigger.update());
 
-	// Define a function to run at each animation frame
-	const scrollFn = (time) => {
-		lenis.raf(time); // Run Lenis' requestAnimationFrame method
-		requestAnimationFrame(scrollFn); // Recursively call scrollFn on each frame
-	};
-	// Start the animation frame loop
-	requestAnimationFrame(scrollFn);
+    // Define a function to run at each animation frame
+    const scrollFn = (time) => {
+        lenis.raf(time); // Run Lenis' requestAnimationFrame method
+        requestAnimationFrame(scrollFn); // Recursively call scrollFn on each frame
+    };
+    // Start the animation frame loop
+    requestAnimationFrame(scrollFn);
 };
 
 // Function to trigger Flip animations when scrolling
 const triggerFlipOnScroll = (galleryEl, options) => {
-	// Default settings for Flip and ScrollTrigger
-	let settings = {
-		flip: {
-			absoluteOnLeave: false,
-			absolute: false,
-			scale: true,
-			simple: true,
-			//...
-		},
-		scrollTrigger: {
-			start: 'center center',
-			end: '+=300%',
-		},
-		stagger: 0
-	};
+    // Default settings for Flip and ScrollTrigger
+    let settings = {
+        flip: {
+            absoluteOnLeave: false,
+            absolute: false,
+            scale: true,
+            simple: true,
+            //...
+        },
+        scrollTrigger: {
+            start: 'center center',
+            end: '+=300%',
+        },
+        stagger: 0
+    };
 
-	// Merge default settings with options provided when calling the function
-	settings = Object.assign({}, settings, options);
+    // Merge default settings with options provided when calling the function
+    settings = Object.assign({}, settings, options);
 
-	// Select elements within the gallery that will be animated
-	const galleryCaption = galleryEl.querySelector('.caption');
-	const galleryItems = galleryEl.querySelectorAll('.gallery__item');
-	const galleryItemsInner = [...galleryItems].map(item => item.children.length > 0 ? [...item.children] : []).flat();
+    // Select elements within the gallery that will be animated
+    const galleryCaption = galleryEl.querySelector('.caption');
+    const galleryItems = galleryEl.querySelectorAll('.gallery__item');
+    const galleryItemsInner = [...galleryItems].map(item => item.children.length > 0 ? [...item.children] : []).flat();
 
-	// Temporarily add the final class to capture the final state
-	galleryEl.classList.add('gallery--switch');
-	const flipstate = Flip.getState([galleryItems, galleryCaption], { props: 'filter, opacity' });
+    // Temporarily add the final class to capture the final state
+    galleryEl.classList.add('gallery--switch');
+    const flipstate = Flip.getState([galleryItems, galleryCaption], { props: 'filter, opacity' });
 
-	// Remove the final class to revert to the initial state
-	galleryEl.classList.remove('gallery--switch');
+    // Remove the final class to revert to the initial state
+    galleryEl.classList.remove('gallery--switch');
 
-	// Create the Flip animation timeline
-	const tl = Flip.to(flipstate, {
-		ease: 'none',
-		absoluteOnLeave: settings.flip.absoluteOnLeave,
-		absolute: settings.flip.absolute,
-		scale: settings.flip.scale,
-		simple: settings.flip.simple,
-		scrollTrigger: {
-			trigger: galleryEl,
-			start: settings.scrollTrigger.start,
-			end: settings.scrollTrigger.end,
-			pin: galleryEl.parentNode,
-			scrub: true,
-		},
-		stagger: settings.stagger
-	});
+    // Create the Flip animation timeline
+    const tl = Flip.to(flipstate, {
+        ease: 'none',
+        absoluteOnLeave: settings.flip.absoluteOnLeave,
+        absolute: settings.flip.absolute,
+        scale: settings.flip.scale,
+        simple: settings.flip.simple,
+        scrollTrigger: {
+            trigger: galleryEl,
+            start: settings.scrollTrigger.start,
+            end: settings.scrollTrigger.end,
+            pin: galleryEl.parentNode,
+            scrub: true,
+        },
+        stagger: settings.stagger
+    });
 
-	// If there are inner elements in the gallery items, animate them too
-	if (galleryItemsInner.length) {
-		tl.fromTo(galleryItemsInner, {
-			scale: 2
-		}, {
-			scale: 1,
-			scrollTrigger: {
-				trigger: galleryEl,
-				start: settings.scrollTrigger.start,
-				end: settings.scrollTrigger.end,
-				scrub: true,
-			},
-		}, 0)
-	}
+    // If there are inner elements in the gallery items, animate them too
+    if (galleryItemsInner.length) {
+        tl.fromTo(galleryItemsInner, {
+            scale: 2
+        }, {
+            scale: 1,
+            scrollTrigger: {
+                trigger: galleryEl,
+                start: settings.scrollTrigger.start,
+                end: settings.scrollTrigger.end,
+                scrub: true,
+            },
+        }, 0)
+    }
 };
 
 // Function to apply scroll-triggered animations to various galleries
 // Apply scroll-triggered animations to each gallery with specific settings
 const scroll = () => {
-	// Define the gallery IDs and their options
-	const galleries = [
-		// { id: '#gallery-1', options: { flip: { absoluteOnLeave: true, scale: false } } },
-		// { id: '#gallery-2' },
-		{ id: '#gallery-3', options: { flip: { absolute: true, scale: false }, scrollTrigger: { start: 'center center', end: '+=900%' }, stagger: 0.05 } },
-		// { id: '#gallery-4' },
-		// { id: '#gallery-5' },
-		{ id: '#gallery-6' },
-		// { id: '#gallery-7' },
-		// { id: '#gallery-8', options: { flip: { scale: false } } },
-		// { id: '#gallery-9' },
-	];
+    // Define the gallery IDs and their options
+    const galleries = [
+        // { id: '#gallery-1', options: { flip: { absoluteOnLeave: true, scale: false } } },
+        // { id: '#gallery-2' },
+        { id: '#gallery-3', options: { flip: { absolute: true, scale: false }, scrollTrigger: { start: 'center center', end: '+=900%' }, stagger: 0.05 } },
+        // { id: '#gallery-4' },
+        // { id: '#gallery-5' },
+        { id: '#gallery-6' },
+        // { id: '#gallery-7' },
+        // { id: '#gallery-8', options: { flip: { scale: false } } },
+        // { id: '#gallery-9' },
+    ];
 
-	// Loop through the galleries and apply the scroll-triggered animations
-	galleries.forEach(gallery => {
-		const galleryElement = document.querySelector(gallery.id);
-		triggerFlipOnScroll(galleryElement, gallery.options);
-	});
+    // Loop through the galleries and apply the scroll-triggered animations
+    galleries.forEach(gallery => {
+        const galleryElement = document.querySelector(gallery.id);
+        triggerFlipOnScroll(galleryElement, gallery.options);
+    });
 }
 
 // Preload images, initialize smooth scrolling, apply scroll-triggered animations, and remove loading class from body
 preloadImages('.gallery__item').then(() => {
-	initSmoothScrolling();
-	scroll();
-	document.body.classList.remove('loading');
+    initSmoothScrolling();
+    scroll();
+    document.body.classList.remove('loading');
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-	var tl = gsap.timeline({
-		paused: "true"
-	});
-	tl.to(".menu", {
-		duration: 1,
-		x: "0%",
-		ease: Expo.easeInOut
-	});
-	tl.fromTo(".li", {
-		y: "-100%",
-		opacity: 0
-	}, {
-		duration: .5,
-		opacity: 1,
-		y: "0%",
-		stagger: 0.25
-	});
-	tl.fromTo(".social-li", {
-		y: "-50%",
-		opacity: 0
-	}, {
-		duration: 0.3,
-		opacity: 1,
-		stagger: 0.25,
-		ease: Expo.easeOut
-	},
-		"-=0.5");
+    var tl = gsap.timeline({
+        paused: "true"
+    });
+    tl.to(".menu", {
+        duration: 1,
+        x: "0%",
+        ease: Expo.easeInOut
+    });
+    tl.fromTo(".li", {
+        y: "-100%",
+        opacity: 0
+    }, {
+        duration: .5,
+        opacity: 1,
+        y: "0%",
+        stagger: 0.25
+    });
+    tl.fromTo(".social-li", {
+        y: "-50%",
+        opacity: 0
+    }, {
+        duration: 0.3,
+        opacity: 1,
+        stagger: 0.25,
+        ease: Expo.easeOut
+    },
+        "-=0.5");
 
-	function toggle() {
-		tl.play();
-	}
+    function toggle() {
+        tl.play();
+    }
 
-	function togglec() {
-		tl.reverse();
-	}
+    function togglec() {
+        tl.reverse();
+    }
 
-	const menuButton = document.getElementById('menuButton');
-	const closeButton = document.getElementById('closeButton');
+    const menuButton = document.getElementById('menuButton');
+    const closeButton = document.getElementById('closeButton');
 
-	if (menuButton) {
-		menuButton.addEventListener('click', toggle);
-	}
-	if (closeButton) {
-		closeButton.addEventListener('click', togglec);
-	}
+    if (menuButton) {
+        menuButton.addEventListener('click', toggle);
+    }
+    if (closeButton) {
+        closeButton.addEventListener('click', togglec);
+    }
 });
 
 
